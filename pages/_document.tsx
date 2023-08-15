5170d8225d80a034169e6aeb3d44f26429f1c4d7
Daniel Sungwon Lee <danlee333@gmail.com>
2023-08-15 13:43:23 -0700

    metadata and logos update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>CoSpace</title>
+        <meta name="title" property="og:title" content="CoSpace" />
+        <meta name="description" property="og:description" content="Management app" />
+        <meta name="image" property="og:image" content="/images/cospace.png" />
+        <link rel="icon" href="/images/cospace.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
