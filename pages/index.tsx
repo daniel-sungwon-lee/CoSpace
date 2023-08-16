266f0116d3f9a8e733f2f6263175c4051002ee56
Daniel Sungwon Lee <danlee333@gmail.com>
2023-08-15 18:37:01 -0700

    font, hero, and global styling update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+import Image from 'next/image'
 
-const inter = Inter({ subsets: ['latin'] })
+const ProductSans = localFont({src: '../public/fonts/ProductSans-Regular.ttf'})
 
 export default function Home() {
   return (
-    <main>
-
+    <main className={`${ProductSans.className}`}>
+      <div className='flex justify-around items-center'>
+        <div className='text-7xl font-bold text-center mb-4'>CoSpace</div>
+        <div className='flex justify-center'>
+          <Image src='/images/cospace.svg' alt='CoSpace logo' width={300} height={300}
+           draggable={false} />
+        </div>
+      </div>
     </main>
   )
 }
