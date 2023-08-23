3d977216a66118a41ff2c3d17b75513d31625508
Daniel Sungwon Lee <danlee333@gmail.com>
2023-08-23 09:27:43 -0700

    changes to calendar

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import localFont from 'next/font/local';
 import { PostAddRounded } from "@mui/icons-material";
 import { Button, Card, CardContent, List } from "@mui/material";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { DateRangeCalendar } from "@mui/x-date-pickers-pro/DateRangeCalendar";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import Post from "@/components/post";
 
+const ProductSans = localFont({ src: '../public/fonts/ProductSans-Regular.ttf' })
+
 export default function Dashboard () {
   const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+
+  })
 
   return (
     <>
       <div className="flex flex-col items-center mt-[7rem] mb-[8rem]">
-        <div className="flex items-center w-[100%] justify-around mb-12">
-          <div>
+        <div className="flex items-center w-[100%] justify-around mb-12 flex-col">
+          <div className='w-[80%] mb-4'>
             <h1 className="text-4xl mb-4">Hello User</h1>
             <h2 className="text-2xl">Nothing scheduled for today. 1 upcoming this week.</h2>
           </div>
-          <div>
+
+          <div className='m-12'>Scheduled stuff here</div>
+
+          <div className='flex justify-center'>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DateCalendar />
+              <DateRangeCalendar calendars={1 /* change to two in desktop (screenWidth) */} />
             </LocalizationProvider>
           </div>
         </div>
